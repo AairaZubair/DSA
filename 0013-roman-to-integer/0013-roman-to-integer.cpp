7b5c1daed452a2835c60d94ab86370e38d170742
Aaira Zubair <aairazubair20@gmail.com>
2026-07-24 19:22:41 +0530

    Time: 0 ms (100%), Space: 10.1 MB (86.89%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    int value(char c)
+    {
+        if(c=='I') return 1;
+        if(c=='V') return 5;
+        if(c=='X') return 10;
+        if(c=='L') return 50;
+        if(c=='C') return 100;
+        if(c=='D') return 500;
+        return 1000;   
+    }
+
+    int romanToInt(string s) {
+
+        int answer = 0;
+
+        for(int i=0;i<s.size();i++)
+        {
+            int current = value(s[i]);
+
+            if(i < s.size()-1)
+            {
+                int next = value(s[i+1]);
+
+                if(current < next)
+                    answer -= current;
+                else
+                    answer += current;
+            }
+            else
+            {
+                answer += current;
+            }
+        }
+
+        return answer;
+    }
+};
